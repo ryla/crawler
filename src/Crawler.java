27d6e861baa9dd69a87babfb8e638cdadc32362a
Riley Alethea Butler <riley.butler@students.olin.edu>
2013-03-09 17:41:18 -0500

    Derpy fetch

--- src/Crawler.java
+++ src/Crawler.java
@@ -1,4 +1,7 @@
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.net.HttpURLConnection;
 import java.util.Set;
 import java.util.HashSet;
 
@@ -18,6 +21,25 @@ public class Crawler {
 		this.rawContents = "";
 		this.url = url;
 		this.urlSet = new HashSet<URL>();
+	}
+	
+	public void fetch() {
+		HttpURLConnection conn = null;
+		try {
+			conn = (HttpURLConnection) this.url.openConnection();
+			conn.connect();
+			InputStream in = conn.getInputStream();
+			int value = in.read();
+			while (value != -1){
+				value = in.read();
+				this.rawContents += (char) value;
+			}
+		} catch (IOException e) {
+			this.rawContents = "";
+		} finally {
+			conn.disconnect();
+		}
+		System.out.print(this.rawContents);
 	}
 	
 	public Set<URL> getURLSet() {
